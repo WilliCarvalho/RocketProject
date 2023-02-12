014f17eb37f6d902042e49aed71ebe066377acbc
WilliCarvalho <wcarvalho@kokku.com.br>
2023-02-11 21:23:51 -0300

    first commit with a few assets

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float mainThrust;
+    [SerializeField] private float rotateThrust;
+
+    private Rigidbody rigidBody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }        
+    }
+
+    private void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            AddRotation(rotateThrust);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            AddRotation(-rotateThrust);
+        }
+    }
+
+    private void AddRotation(float rotationValue)
+    {
+        transform.Rotate(Vector3.forward * rotationValue * Time.deltaTime);
+    }
+}
